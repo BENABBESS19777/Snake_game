60a850f3ac777620cc4930b576bfa08e3afee49d
BEN ABBESS MED ALI <benabbessmedali@gmail.com>
2024-12-06 23:37:14 +0000

    Final part

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
-import random
+
 
 # Set up the screen =====================================================
 screen = Screen()
@@ -12,6 +14,8 @@ screen.tracer(0)
 
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -24,14 +28,22 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
-   
-
-
-
-
-
-
-
+    #detect collision with food
+    if snake.head.distance(food) < 15 :
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+    # detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.game_over()
+        game_is_on = False
+    # detect collision with trail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.game_over()
+            game_is_on = False
 
 
 screen.exitonclick()
